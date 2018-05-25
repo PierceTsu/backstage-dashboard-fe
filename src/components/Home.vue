8279d78a38ec8ee469a88e00c6bcc8578a313b6f
pierce <piercetsu@foxmail.com>
2018-05-25 11:10:59 +0800

    init vue element-ui project

---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-container id="container">
+    <el-aside><sidebar/></el-aside>
+    <el-container>
+      <el-header><top/></el-header>
+      <el-main>
+        Main
+      </el-main>
+      <el-footer><bottom/></el-footer>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import top from './top/index'
+import bottom from './footer/index'
+import sidebar from './sidebar/index'
+export default {
+  name: 'Home',
+  components: {top, bottom, sidebar},
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #container {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+  }
+</style>
---
+++ src/components/footer/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h5 style="text-align: center">2018 © Pierce™ yaoyuehome.com All rights reserved.</h5>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<style scoped>
+
+</style>
